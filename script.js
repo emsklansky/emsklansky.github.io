6762acdea0ce8180e6d2110dd88bcb7dec454c37
emsklansky <68756542+emsklansky@users.noreply.github.com>
2020-07-24 23:09:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 
 let TimerLength = 45;
 let seconds = 45;
+let hundredths = 00;
 
+//Define vars to hold "display" value
+let displayHundredths = 0;
+let displaySeconds = 0;
 
 //Define var to hold set interval function
 let interval = null;
@@ -12,11 +16,32 @@ let status = 'stopped';
 
 //Timer function (logic to determine when to increment next value, etc.)
 function Timer(){
+    
+    hundredths--;
 
-seconds--;
+    if(hundredths === -1){
+        seconds--;
+        hundredths = 99;
+    }
+
+    //If seconds/hundredths are only one digit, add leading 0 to the value
+
+    if(seconds<10){
+        displaySeconds = "0" + seconds.toString();
+    }
+    else{
+        displaySeconds = seconds;
+    }
+
+    if(hundredths<10){
+        displayHundredths = "0" + hundredths.toString();
+    }
+    else{
+        displayHundredths = hundredths;
+    }
 
 //Display updated time value to user
-document.getElementById("display").innerHTML = seconds;
+document.getElementById("display").innerHTML = displaySeconds + ":" + displayHundredths;
 }
 
 //show beginning time to user
@@ -31,7 +56,7 @@ function StartStop(){
         if(status === "stopped"){
 
         //start the stopwatch (by calling the setInterval() funciton)
-        interval = window.setInterval(Timer, 1000);
+        interval = window.setInterval(Timer, 10);
         document.getElementById("StartStop").innerHTML = "Stop";
         status = "Started";
         }
@@ -51,8 +76,10 @@ function Reset(){
 
     window.clearInterval(interval);
     seconds = TimerLength;
-    document.getElementById("display").innerHTML = seconds
+    hundredths = 0;
+    document.getElementById("display").innerHTML = seconds + ":" + "00"
     document.getElementById("StartStop").innerHTML = "Start"
+    status = "stopped";
 }
 
 function getInput() {
